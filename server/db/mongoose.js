52835598178a3279a74aaaa10771b7a7517e543a
Vishal_team <Vishal.j@myteam11.com>
2018-08-09 12:47:06 +0530

    server mongo test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
-
+//mongodb://<dbuser>:<dbpassword>@ds031741.mlab.com:31741/todoapp
+//mongodb://localhost:27017/ToDoApp
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ToDoApp',{ useNewUrlParser: true });
+mongoose.connect('mongodb://vjandvj:vj1992@123098@ds031741.mlab.com:31741/todoapp',{ useNewUrlParser: true });
 
 module.exports = {
     mongoose
