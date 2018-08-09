c14e700309f7086a35e6b8daea5df97f39a4013d
Vishal_team <Vishal.j@myteam11.com>
2018-08-09 12:51:53 +0530

    chnges

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 //mongodb://<dbuser>:<dbpassword>@ds031741.mlab.com:31741/todoapp
 //mongodb://localhost:27017/ToDoApp
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://vjandvj:vj1992@123098@ds031741.mlab.com:31741/todoapp',{ useNewUrlParser: true });
+mongoose.connect('mongodb://vjandvj:vj1992@ds031741.mlab.com:31741/todoapp',{ useNewUrlParser: true });
 
 module.exports = {
     mongoose
